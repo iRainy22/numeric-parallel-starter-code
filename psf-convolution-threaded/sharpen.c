e5f5b76724db744876046fc2b75eec47de873a30
iRainy22 <rainier.ring@gmail.com>
2026-09-09 18:56:32 -0700

    Update Sharpen.c with OMP

--- psf-convolution-threaded/sharpen.c
+++ psf-convolution-threaded/sharpen.c
@@ -15,12 +15,12 @@
 //#define IMG_HEIGHT (300)
 //#define IMG_WIDTH (400)
 
-#define IMG_HEIGHT (3000)
-#define IMG_WIDTH (4000)
+#define IMG_HEIGHT (960)
+
+#define IMG_WIDTH (1280)
 
 #define HEADER_SIZE (40)
-
-#define ITERATIONS (90)
+#define ITERATIONS (1000)
 
 #define FAST_IO
 
@@ -54,7 +54,7 @@ FLOAT PSF[9] = {-K/F, -K/F, -K/F, -K/F, K+1.0, -K/F, -K/F, -K/F, -K/F};
 
 int main(int argc, char *argv[])
 {
-    int fdin, fdout, bytesRead=0, bytesWritten=0, bytesLeft, i, j, iter, rc, pixel, readcnt=0, writecnt=0;
+    int fdin, fdout, bytesRead=0, bytesWritten=0, bytesLeft, i, j, iter, rc, pixel, readcnt=0, writecnt=0, thread_count;
     UINT64 microsecs=0, millisecs=0;
     FLOAT temp, fstart, fnow;
     struct timespec start, now;
@@ -62,9 +62,9 @@ int main(int argc, char *argv[])
     clock_gettime(CLOCK_MONOTONIC, &start);
     fstart = (FLOAT)start.tv_sec  + (FLOAT)start.tv_nsec / 1000000000.0;
     
-    if(argc < 3)
+    if(argc < 4)
     {
-       printf("Usage: sharpen input_file.ppm output_file.ppm\n");
+       printf("Usage: sharpen input_file.ppm output_file.ppm <thread count>\n");
        exit(-1);
     }
     else
@@ -81,7 +81,8 @@ int main(int argc, char *argv[])
             printf("Error opening %s\n", argv[1]);
         }
         //else
-        //    printf("Output file=%s opened successfully\n", "sharpen.ppm");
+        //printf("Output file=%s opened successfully\n", "sharpen.ppm");
+        sscanf(argv[3],"%d", &thread_count);
     }
 
     bytesLeft=HEADER_SIZE-1;
@@ -144,9 +145,9 @@ int main(int argc, char *argv[])
     clock_gettime(CLOCK_MONOTONIC, &now);
     fnow = (FLOAT)now.tv_sec  + (FLOAT)now.tv_nsec / 1000000000.0;
     printf("\nstart test at %lf\n", fnow-fstart);
-
     for(iter=0; iter < ITERATIONS; iter++)
     {
+        #pragma omp parallel for num_threads(thread_count)
         // Skip first and last row, no neighbors to convolve with
         for(i=1; i<((IMG_HEIGHT)-1); i++)
         {
@@ -197,7 +198,6 @@ int main(int argc, char *argv[])
 	        convB[(i*IMG_WIDTH)+j]=(UINT8)temp;
             }
         }
-
     }
 
     clock_gettime(CLOCK_MONOTONIC, &now);
